7464cb4b2a8a0adc67b45dccb757023c6aa2eb68
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2024-12-20 11:17:47 +0100

    Fix failing tests

--- replicant/test/core_test.cljs
+++ replicant/test/core_test.cljs
@@ -5,37 +5,40 @@
 (deftest calculating-state
   (testing "correct answer"
     (testing "on time"
-      (is (= {:question [1 2]
+      (is (= {:answer nil
+              :question [1 2]
               :score 2
               :high-score 2
               :mode :against-the-clock
               :wrongly-answered #{}
               :deadline-passed? false}
-             (process-answer {:answer 6
+             (process-answer {:answer "6"
                               :question [2 3]
                               :score 1
                               :high-score 1
                               :wrongly-answered #{}} [1 2]))))
     (testing "too late"
-      (is (= {:question [1 2]
+      (is (= {:answer nil
+              :question [1 2]
               :score 0
               :high-score 1
               :mode :against-the-clock
               :wrongly-answered #{[2 3]}
               :deadline-passed? false}
-             (process-answer {:answer 6
+             (process-answer {:answer "6"
                               :question [2 3]
                               :high-score 1
                               :wrongly-answered #{}
                               :deadline-passed? true} [1 2])))))
    (testing "wrong answer"
-     (is (= {:question [2 3]
+     (is (= {:answer nil
+             :question [2 3]
              :score 0
              :high-score 10
              :mode :show-correct-answer
              :wrongly-answered #{[2 3]}
              :deadline-passed? false}
-            (process-answer {:answer 5
+            (process-answer {:answer "5"
                              :question [2 3]
                              :score 5
                              :high-score 10
